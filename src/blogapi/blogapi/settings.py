1a2b32f0ada3d4e886b3bcda9b5580b2f8a283a3
Antoine Reversat <a.reversat@gmail.com>
2019-02-01 15:06:29 -0500

    Allow all in CORS header

--- src/blogapi/blogapi/settings.py
+++ src/blogapi/blogapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'blogapi.blog',
 ]
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,6 +59,7 @@ REST_FRAMEWORK = {
     ]
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'blogapi.urls'
 
 TEMPLATES = [
