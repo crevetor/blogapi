70a4f11c6c24f79640c4c5cff7f0249f1ca9dca9
Antoine Reversat <a.reversat@gmail.com>
2019-02-05 10:12:31 -0500

    Add the photo part to the user admin

--- src/blogapi/blogapi/blog/admin.py
+++ src/blogapi/blogapi/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from blogapi.blog.models import Tag, Post
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from blogapi.blog.models import Tag, Post, Author
+
+class AuthorInline(admin.StackedInline):
+    model = Author
+    can_delete = False
+    verbose_name_plural = 'author'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (AuthorInline,)
 
 class PostAdmin(admin.ModelAdmin):
     pass
@@ -7,5 +17,7 @@ class PostAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     pass
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
