fdc201ac2c252a4785703b9d989d26616beed155
Antoine Reversat <a.reversat@gmail.com>
2020-10-22 10:37:53 -0400

    Use environment variables for settings that are different in prod.

--- src/blogapi/blogapi/settings.py
+++ src/blogapi/blogapi/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!(l$$oq31k&kl*%89tm$4pl(*x7h#^vz9=hj0_47yv9uz+ucjg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '!(l$$oq31k&kl*%89tm$4pl(*x7h#^vz9=hj0_47yv9uz+ucjg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogapi.crevetor.org']
 
 
 # Application definition
@@ -127,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
